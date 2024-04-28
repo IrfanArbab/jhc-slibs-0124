fb6920b194dfb8808606841b6005e72bd341125a
IrfanArbab <105029258+IrfanArbab@users.noreply.github.com>
2024-04-28 15:16:58 +0530

    Create buildAndPushDockerImage.groovy

---
+++ vars/buildAndPushDockerImage.groovy
@@ -0,0 +1,48 @@
+def call(String imageName, String awsAccountId, String awsRegion) {
+    pipeline {
+        agent any
+      
+        stages {
+            stage('Authenticate with AWS ECR') {
+                steps {
+                    script {
+                        withCredentials([[
+                            $class: 'AmazonWebServicesCredentialsBinding',
+                            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                            credentialsId: 'K8s',
+                            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                        ]]) {
+                            bat "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
+                        }
+                    }
+                }
+            }
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        docker.build("${imageName}")
+                    }
+                }
+            }
+            stage('Push Docker Image to AWS ECR') {
+                steps {
+                    script {
+                        docker.withRegistry("https://${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}") {
+                            docker.image("${imageName}").push('latest')
+                        }
+                    }
+                }
+            }
+            stage('Pull and Run docker image') {
+                steps {
+                    script {
+                        bat "docker pull ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:latest"
+                        bat "docker run -d -p 4200:4200 ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:latest"
+                        bat 'docker ps'
+                        bat 'docker ps -a'
+                    }
+                }
+            }
+        }
+    }
+}
